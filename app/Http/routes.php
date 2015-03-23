12f449ec21c7aada6b2b54ad1bd345afe7ed871a
maxfine <max_fine@qq.com>
2015-03-24 00:08:20 +0800

    Route
    Route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,5 +32,4 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth
   Route::get('/', 'AdminHomeController@index');
   Route::resource('pages', 'PagesController');
   Route::get('pages/{id?}', 'PagesController@show')->where(array('id'=>'[0-9]+'));
-  Route::get('pages/{id?}/edit', 'PagesController@edit')->where(array('id'=>'[0-9]+'));
 });
