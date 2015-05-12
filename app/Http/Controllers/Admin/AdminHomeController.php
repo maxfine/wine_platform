e8e99cf81fc73c6a5ad3c5f58ac463d15db80bf8
maxfine <max_fine@qq.com>
2015-05-12 10:00:29 +0800

    admin
    admin

--- app/Http/Controllers/Admin/AdminHomeController.php
+++ app/Http/Controllers/Admin/AdminHomeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Page;
+use App\Models\ArticleCat;
 
 class AdminHomeController extends Controller {
 
@@ -16,7 +17,7 @@ class AdminHomeController extends Controller {
 	 */
 	public function index()
 	{
-        return view('AdminHome')->withPages(Page::all());
+        return view('AdminHome')->with('pages',Page::all())->with('articleCats',ArticleCat::all());
 	}
 
 	/**
--- resources/views/AdminHome.blade.php
+++ resources/views/AdminHome.blade.php
@@ -5,7 +5,7 @@
   <div class="row">
     <div class="col-md-10 col-md-offset-1">
       <div class="panel panel-default">
-        <div class="panel-heading">后台首页</div>
+        <div class="panel-heading">单页</div>
 
         <!-- pages -->
         <div class="panel-body">
@@ -34,24 +34,33 @@
         </div>
 
         
+
+      </div>
+    </div>
+  </div>
+
+<div class="row">
+    <div class="col-md-10 col-md-offset-1">
+      <div class="panel panel-default">
+        <div class="panel-heading">文章栏目</div>
         <!-- articles -->
         <div class="panel-body">
 
-        <a href="{{ URL('admin/article_cats/create') }}" class="btn btn-lg btn-primary">新增</a>
+        <a href="{{ URL('admin/article_cats/create') }}" class="btn btn-lg btn-primary">新增栏目</a>
 
-          @foreach ($article_cats $article_cat)
+          @foreach ($articleCats as $articleCat)
             <hr>
             <div class="page">
-              <h4>{{ $article_cat->cat_name }}</h4>
+              <h4>{{ $articleCat->cat_name }}</h4>
               <div class="content">
                 <p>
-                  {{ $article_cat->body }}
+                  {{ $articleCat->cat_brief }}
                 </p>
               </div>
             </div>
-            <a href="{{ URL('admin/pages/'.$page->id.'/edit') }}" class="btn btn-success">编辑</a>
+            <a href="{{ URL('admin/article_cats/'.$articleCat->id.'/edit') }}" class="btn btn-success">编辑</a>
 
-            <form action="{{ URL('admin/pages/'.$page->id) }}" method="POST" style="display: inline;">
+            <form action="{{ URL('admin/article_cats/'.$articleCat->id) }}" method="POST" style="display: inline;">
               <input name="_method" type="hidden" value="DELETE">
               <input type="hidden" name="_token" value="{{ csrf_token() }}">
               <button type="submit" class="btn btn-danger">删除</button>
