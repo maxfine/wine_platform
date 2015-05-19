1815279586bc6e7d06ff93e59d16662966614531
maxfine <max_fine@qq.com>
2015-05-19 19:29:25 +0800

    admin
    admin

--- public/test.php
+++ public/test.php
@@ -34,4 +34,37 @@ class A{
 
 $a = new A(); echo $a->t;
 ***********END*************/
-phpinfo();
+
+//phpinfo();
+
+/*
+ * --------------------------------------
+ * 反射机制
+ * --------------------------------------
+ */
+class testClass
+{
+    public function testFunc($param1, $param2=0)
+    {
+ 
+    }
+}
+//reflectionMethod 反射类，该类报告了一个访法的有关信息
+$method = new ReflectionMethod('testClass','testFunc');
+ 
+$params = $method->getParameters();
+foreach ($params as $param) 
+{
+      echo "param name:".$param->getName()."<BR><BR>";
+      if($param->isOptional())
+      {
+           echo "Default value:".$param->getDefaultValue()."<BR><BR>";
+      } 
+      if($param->allowsNull() ===true)
+      {
+          echo "可以为空";
+      }else{
+          echo "不能为空";
+      }
+      echo "<BR>=================<BR>";
+}
