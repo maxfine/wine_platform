aa1e844adc164f99cb1daf743086002396eee019
maxfine <max_fine@qq.com>
2015-07-30 17:48:24 +0800

    payments
    payments

--- app/Http/Controllers/Dome/PaymentsController.php
+++ app/Http/Controllers/Dome/PaymentsController.php
@@ -15,6 +15,7 @@ class PaymentsController extends Controller {
     private $subject;
     private $bankcode;
     private $quantity;
+    public  $options;
 
     public function __construct(){
 
@@ -57,10 +58,11 @@ class PaymentsController extends Controller {
 
         $this->price = Input::get('price');
         $this->sn = $this->createSn();
-        $this->subject = $this->getSubject();
+        $this->subject = $this->createSubject();
         $this->bankcode = Input::get('bankcode');
         $this->quantity = Input::get('quantity');
         $this->setPayway(Input::get('payway'), $this->bankcode);
+        $this->payway = Input::get('payway');
 
         //进行第三方支付网站跳转
         \Omnipay::setGateway($this->payway);
@@ -138,80 +140,17 @@ class PaymentsController extends Controller {
      * 返回成功提示,5秒后跳转到指定页面
      */
     public function respondGet($code = ''){
-        //支付宝服务器发送的数据
-        /**
-        $buyer_email=1526469221%40qq.com; //买家支付宝邮箱
-        $buyer_id=2088002529453464; //买家支付宝id
-        $exterface=create_direct_pay_by_user; //支付接口类型
-        $is_success=T; //是否成功
-        $notify_id=RqPnCoPT3K9%252Fvwbh3InSPgy8T3pzxoGMaFYGnySYx%252FPIXtRxZfwpAmqCwjXGOTw44mM1; //发送id
-        $notify_time=2015-07-29+16%3A17%3A20; //服务端数据发送时间
-        $notify_type=trade_status_sync; //服务端发送数据类型:同步发送
-        $out_trade_no=2015072996300; //商户订单号
-        $payment_type=1; //付款类型
-        $seller_email=437630959%40qq.com; //卖家支付宝邮箱
-        $seller_id=2088021297824829; //卖家id
-        $subject=%E4%BA%A7%E5%93%81%E6%94%AF%E4%BB%98; //主题
-        $total_fee=0.01; //价格
-        $trade_no=2015072900001000460059090687; //支付宝订单号
-        $trade_status=TRADE_SUCCESS; //支付状态
-        $sign=d79ba11387d379256aafa72e64485d9a; //签名
-        $sign_type=MD5; //签名加密方式
-        **/
-
-        if(empty($code)){
-            return false;
-        }
-
-        $seller_email = rawurldecode($_GET['seller_email']);
-        $order_sn = str_replace($_GET['subject'], '', $_GET['out_trade_no']);
-        $order_sn = trim($order_sn);
-
-        /* 检查数字签名是否正确 */
-        ksort($_GET);
-        reset($_GET);
-
-        $sign = '';
-        foreach ($_GET AS $key=>$val)
-        {
-            if ($key != 'sign' && $key != 'sign_type' && $key != 'code')
-            {
-                $sign .= "$key=$val&";
-            }
-        }
-
-        $sign = substr($sign, 0, -1) . 'y7ltjfmpkfosn5vbi69a5080kcb96nz7';
-        dd(md5($sign));
-
-        $this->price = Input::get('total_fee');
-        $this->sn = Input::get('out_trade_no');
-        $this->subject = Input::get('subject');
-        $this->bankcode = Input::get('bankcode');
-        $this->quantity = Input::get('quantity');
-        $this->setPayway($code, $this->bankcode);
+        if(empty($code))return false;
 
         \Omnipay::setGateway($code);
-        $options = $this->getOptions();
-        $resquest = \Omnipay::purchase($options);
-        dd($resquest->getData());
-        $response = \Omnipay::purchase($options)->send(); //此send为父类的send方法
-        $redirectData = $response->getRedirectData();
-        dd($redirectData);
-        dd(\Omnipay::gateway());
-
-        //数据
-
-        //验证数据
-
-        if($avi == true){
-            //验证成功
+        $resquest = \Omnipay::completePurchase(['request_params' => \Input::all()]);
+        $response = $resquest->send();
+        if($response->isSuccessful()){
+            //验证成功to-done
 
         }else{
-            //验证失败
-            dd('支付失败');
+
         }
-
-
     }
 
     /**
@@ -248,7 +187,7 @@ class PaymentsController extends Controller {
      * --------------------------------------------------------------
      * @return string
      */
-    private function getSubject(){
+    private function createSubject(){
         return '产品支付';
     }
 
@@ -262,11 +201,7 @@ class PaymentsController extends Controller {
 
         if(!$paywayTemp) return $payway;
 
-        if($paywayTemp == 'Alipay_Bank' && !$bankcode){
-            $payway = 'Alipay_Express';
-        }else{
-            $payway = $paywayTemp;
-        }
+        $payway = $paywayTemp;
 
         $this->payway = $payway;
     }
